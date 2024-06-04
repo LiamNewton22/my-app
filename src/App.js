0c80a96ad603c716e0e9c3c5d149415be6bf1f45
liam <newtonliam03@gmail.com>
2024-06-04 22:43:42 +1000

    updatelogin

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import BrowserRouter, Routes, and Route
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Components/Login';
 import HomeAdminPage from './Components/HomeAdminPage';
 
 const App = () => {
   return (
-    <Router> {/* Wrap everything in Router */}
+    <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/HomeAdminPage" element={<HomeAdminPage />} />
